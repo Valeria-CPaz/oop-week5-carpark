41bde924afe409ec5bf93075fde5be15f9dd8800
Valeria Paz <21214@student.dorset-college.ie>
2020-10-13 13:37:56 +0100

    second commit

--- src/com/val/Main.java
+++ src/com/val/Main.java
@@ -3,6 +3,26 @@ package com.val;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
     }
+
+    public static double calculateCharges (double minimumFee, double additionalFee, double maxCharge) {
+
+		minimumFee = 2.0;
+		additionalFee = 0.5;
+		maxCharge = 10.00;
+		double totalHours = 0;
+		double totalFee = 0;
+
+		if (totalHours <= 3){
+			System.out.println(minimumFee);
+		} else if (totalHours > 3 && totalHours < 24){
+			totalFee = minimumFee + ((totalHours - 3) * additionalFee);
+		} else {
+			System.out.println(maxCharge);
+		}
+
+
+	}
 }
