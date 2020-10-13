07258f57854490cae0272a813fc6b8582631b207
Valeria Paz <21214@student.dorset-college.ie>
2020-10-13 13:49:46 +0100

    second commit

--- src/com/val/Main.java
+++ src/com/val/Main.java
@@ -2,27 +2,29 @@ package com.val;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-
-    }
-
-    public static double calculateCharges (double minimumFee, double additionalFee, double maxCharge) {
-
-		minimumFee = 2.0;
-		additionalFee = 0.5;
-		maxCharge = 10.00;
-		double totalHours = 0;
-		double totalFee = 0;
-
-		if (totalHours <= 3){
-			System.out.println(minimumFee);
-		} else if (totalHours > 3 && totalHours < 24){
-			totalFee = minimumFee + ((totalHours - 3) * additionalFee);
-		} else {
-			System.out.println(maxCharge);
-		}
-
+		calculateCharges(9);
 
 	}
+
+	public static void calculateCharges(double totalHours) {
+
+		double minimumFee = 2.0;
+		double additionalFee = 0.5;
+		double maxCharge = 10.00;
+		double totalFee = 0;
+
+
+		if (totalHours <= 3) {
+			System.out.println("Total fee for " + totalHours + " hours is: " + minimumFee);
+		} else if (totalHours > 3 && totalHours < 24) {
+			totalFee = minimumFee + ((totalHours - 3) * additionalFee);
+			System.out.println("Total fee for " + totalHours + " hours is: " + totalFee);
+		} else {
+			System.out.println("Total fee for " + totalHours + " hours is: " + maxCharge);
+		}
+
+	}
+
 }
